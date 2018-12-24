8ecb6f2942dfcae18466446243ece13418426fe7
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-12-24 01:01:37 -0500

    set up auth resolvers (lots of errors)

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -18,6 +18,44 @@ const deleteLink = (root, { id }, context) =>
     id
   });
 
+// Authentication
+
+const signup = async (parent, args, context, info) => {
+  // Hash the password
+  const password = await bcrypt.hash(args.password, 10);
+
+  // Create the user
+  const user = await context.prisma.createUser({
+    ...args,
+    password
+  });
+
+  // JWT Auth
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+  return {
+    token,
+    user
+  };
+};
+
+const login = async (parent, args, context, info) => {
+  // Validate user
+  const user = await context.prisma.user({ email: args.email });
+  if (!user) throw new Error("No such user found");
+
+  // Validate password
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) throw new Error("Invalid password");
+
+  // JWT Auth
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user
+  };
+};
+
 module.exports = {
   post,
   updateLink,
