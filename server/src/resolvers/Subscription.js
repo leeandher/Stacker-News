9d518b27494ae932186c8193553d5bc120e9e027
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-12-28 23:26:43 -0500

    set up subscription resolver

---
+++ server/src/resolvers/Subscription.js
@@ -0,0 +1,11 @@
+const newLinkSubscribe = (parent, args, context, info) =>
+  context.prisma.$subscribe.link({ mutation_in: ["CREATED"] }).node();
+
+const newLink = {
+  subscribe: newLinkSubscribe,
+  resolve: payload => payload
+};
+
+module.exports = {
+  newLink
+};
