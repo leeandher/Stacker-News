c2186baaeca54b3c6d8b89b7ca61e77667d4ae85
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-12-30 10:47:45 -0500

    up to auth in tutorial

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
-// Import the dependencies
+// Set up the environment
 require("dotenv").config({ path: "settings.env" });
+
+// Import the dependencies
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 
 // Import the resolvers
 
@@ -11,29 +14,28 @@ let links = [
     description: "Fullstack tutorial for GraphQL"
   }
 ];
-let idCount = links.length;
+
 const resolvers = {
   Query: {
-    info: () => `This is an API of StackerNews, a HackerNews clone`,
-    feed: () => links
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    }
   },
   Mutation: {
-    // 2
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
