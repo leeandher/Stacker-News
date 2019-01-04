0941bed8e462508782cb55224ddedcba62f17087
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-04 11:23:21 -0500

    AHHHHH DEBUGGGED THE CONNECT ERROR!

--- server/src/resolvers/Link.js
+++ server/src/resolvers/Link.js
@@ -1,6 +1,7 @@
 // Resolves the 'User' field on Link type in '../schema.graphql'
-const postedBy = ({ id }, args, context) =>
-  context.prisma.links({ id }).postedBy();
+const postedBy = ({ id }, args, context) => {
+  return context.prisma.link({ id }).postedBy();
+};
 
 module.exports = {
   postedBy
