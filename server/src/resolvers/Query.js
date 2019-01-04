f5af5625f88aac94b99e46d90daf5bf5423846d8
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-04 12:47:31 -0500

    implemented single user/link query

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,17 @@
+const info = () =>
+  `This is a GraphQL Server powering the HackerNews clone: StackerNews!`;
+
 const feed = (parent, args, context, info) => context.prisma.links();
 
+const user = (parent, { userId }, context, info) =>
+  context.prisma.user({ id: userId });
+
+const link = (parent, { linkId }, context, info) =>
+  context.prisma.link({ id: linkId });
+
 module.exports = {
-  feed
+  info,
+  feed,
+  user,
+  link
 };
