885338a2be2a34ba72960af8646f45bbbd11965b
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-07 21:24:18 -0500

    modified home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
-const Header = () => (
-  <header className="flex pal justify-between nowrap orange">
-    <div className="flex flex-fixed black">
-      <div className="fw7 mr1">Stacker News</div>
-      <Link to="/" className="ml1 no-underline black">
-        new
-      </Link>
-      <div className="ml1">|</div>
-      <Link to="/create" className="ml1 no-underline black">
-        submit
-      </Link>
-    </div>
-  </header>
-);
+import { AUTH_TOKEN } from "../constants";
 
+const Header = ({ history }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+  return (
+    <header className="flex pal justify-between nowrap orange">
+      <div className="flex flex-fixed black">
+        <div className="fw7 mr1">Stacker News</div>
+        <Link to="/" className="ml1 no-underline black">
+          new
+        </Link>
+        <div className="ml1">|</div>
+        <Link to="/create" className="ml1 no-underline black">
+          submit
+        </Link>
+      </div>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              history.push("/..");
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link to="/login" className="ml1 no-underline black">
+            login
+          </Link>
+        )}
+      </div>
+    </header>
+  );
+};
 export default withRouter(Header);
