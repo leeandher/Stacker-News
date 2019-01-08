7b36f1e34f6a05f2f117f2f0bdd02ce8b58d096b
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-07 21:42:33 -0500

    implemented weak auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 import { AUTH_TOKEN } from "../constants";
 
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
 class Login extends Component {
   state = {
     // Swtich between Login and Signup
@@ -10,8 +29,10 @@ class Login extends Component {
     name: ""
   };
 
-  _confirm = async () => {
-    // TODO
+  _confirm = async data => {
+    const { token } = this.state.login ? data.login : data.signup;
+    this._saveUserData(token);
+    this.props.history.push("/");
   };
 
   _saveUserData = token => {
@@ -39,16 +60,24 @@ class Login extends Component {
             placeholder="Your email address"
           />
           <input
-            type="text"
+            type="password"
             value={password}
             onChange={e => this.setState({ password: e.target.value })}
             placeholder="Choose a safe password"
           />
         </div>
         <div className="flex mt3">
-          <div className="pointer mr2 button" onClick={() => this._confirm()}>
-            {login ? "login" : "create account"}
-          </div>
+          <Mutation
+            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+            variables={{ email, password, name }}
+            onCompleted={data => this._confirm(data)}
+          >
+            {mutation => (
+              <div className="pointer mr2 button" onClick={mutation}>
+                {login ? "login" : "create account"}
+              </div>
+            )}
+          </Mutation>
           <div
             className="pointer button"
             onClick={() => this.setState({ login: !login })}
