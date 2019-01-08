282a81b33192305c8e6f11bc9e27487053a1c067
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-07 20:14:23 -0500

    set up actual DB query 🎉🎉

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,5 +1,23 @@
+// React dependencies
 import React from "react";
 import Link from "./Link";
+
+// GraphQL dependencies
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
 
 const LinkList = () => {
   const linksToRender = [
@@ -16,11 +34,45 @@ const LinkList = () => {
     }
   ];
   return (
-    <div>
-      {linksToRender.map(link => (
-        <Link key={link.id} link={link} />
-      ))}
-    </div>
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return (
+            <div>
+              <span role="img" aria-label="flexing">
+                💪
+              </span>
+              Fetching...
+              <span role="img" aria-label="flexing">
+                💪
+              </span>
+            </div>
+          );
+        }
+        if (error) {
+          console.error(error);
+          return (
+            <div>
+              <span role="img" aria-label="nope">
+                ❌
+              </span>
+              An error has occured!
+              <span role="img" aria-label="nope">
+                ❌
+              </span>
+            </div>
+          );
+        }
+
+        return (
+          <div>
+            {data.feed.links.map(link => (
+              <Link key={link.id} link={link} />
+            ))}
+          </div>
+        );
+      }}
+    </Query>
   );
 };
 
