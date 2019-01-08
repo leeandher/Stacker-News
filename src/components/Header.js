1b3e37f621b0d16c491dc17b548a935819f1c302
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-07 20:47:41 -0500

    set up router uI

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+
+const Header = () => (
+  <header className="flex pal justify-between nowrap orange">
+    <div className="flex flex-fixed black">
+      <title className="fw7 mr1">Stacker News</title>
+      <Link to="/" className="ml1 no-underline black">
+        new
+      </Link>
+      <div className="ml1">|</div>
+      <Link to="/create" className="ml1 no-underline black">
+        submit
+      </Link>
+    </div>
+  </header>
+);
+
+export default withRouter(Header);
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { BrowserRouter } from "react-router-dom";
 
 import * as serviceWorker from "./serviceWorker";
 
@@ -22,9 +23,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
