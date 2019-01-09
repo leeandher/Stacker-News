0d513e6043e46652bd08a796357b93790ed312a9
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-08 19:42:14 -0500

    setting up cache reloading live

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,5 +1,5 @@
 // React dependencies
-import React from "react";
+import React, { Component } from "react";
 import Link from "./Link";
 
 // GraphQL dependencies
@@ -14,51 +14,82 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `;
 
-const LinkList = () => (
-  <Query query={FEED_QUERY}>
-    {({ loading, error, data }) => {
-      if (loading) {
-        return (
-          <div>
-            <span role="img" aria-label="flexing">
-              💪
-            </span>
-            Fetching...
-            <span role="img" aria-label="flexing">
-              💪
-            </span>
-          </div>
-        );
-      }
-      if (error) {
-        console.error(error);
-        return (
-          <div>
-            <span role="img" aria-label="nope">
-              ❌
-            </span>
-            An error has occured!
-            <span role="img" aria-label="nope">
-              ❌
-            </span>
-          </div>
-        );
-      }
+class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    // Read the cached data for FEED_QUERY from the store
+    const data = store.readQuery({ query: FEED_QUERY });
 
-      return (
-        <div>
-          {data.feed.links.map(link => (
-            <Link key={link.id} link={link} />
-          ))}
-        </div>
-      );
-    }}
-  </Query>
-);
+    // Get the link the user just voted on, and modify the votes on them
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    // Write the new vote quantity back to the store as FEED_QUERY
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return (
+              <div>
+                <span role="img" aria-label="flexing">
+                  💪
+                </span>
+                Fetching...
+                <span role="img" aria-label="flexing">
+                  💪
+                </span>
+              </div>
+            );
+          }
+          if (error) {
+            console.error(error);
+            return (
+              <div>
+                <span role="img" aria-label="nope">
+                  ❌
+                </span>
+                An error has occured!
+                <span role="img" aria-label="nope">
+                  ❌
+                </span>
+              </div>
+            );
+          }
+
+          return (
+            <div>
+              {data.feed.links.map((link, i) => (
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={i}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
 
 export default LinkList;
