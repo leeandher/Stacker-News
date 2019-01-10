18e848f1d1b24f85abab32d4f770f98c12ad94fc
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-09 21:19:05 -0500

    :construction: Implementing pagination

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -6,10 +6,12 @@ import Link from "./Link";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
+import { LINKS_PER_PAGE } from "../constants";
+
 // GQL Calls
 export const FEED_QUERY = gql`
-  {
-    feed {
+  query FeedQuery($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(first: $first, skip: $skip, orderBy: $orderBy) {
       links {
         id
         createdAt
@@ -117,9 +119,20 @@ class LinkList extends Component {
     });
   };
 
+  // Implementing pagination
+  _getQueryVariables = () => {
+    const isNewPage = this.props.location.pathname.includes("new");
+    const page = parseInt(this.props.match.params.page, 10);
+
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+    const first = isNewPage ? LINKS_PER_PAGE : 100;
+    const orderBy = isNewPage ? "createdAt_DESC" : null;
+    return { first, skip, orderBy };
+  };
+
   render() {
     return (
-      <Query query={FEED_QUERY}>
+      <Query query={FEED_QUERY} variables={this._getQueryVariables()}>
         {({ loading, error, data, subscribeToMore }) => {
           if (loading) {
             return (
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
-const AUTH_TOKEN = "auth-token";
+// FRONTEND SECRETS
+const AUTH_TOKEN = "4ur3y350nly";
+const LINKS_PER_PAGE = 5;
 
 module.exports = {
-  AUTH_TOKEN
+  AUTH_TOKEN,
+  LINKS_PER_PAGE
 };
