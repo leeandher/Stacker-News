38fbb0e1d8611b886e48d7fc6413efe617c7d215
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-09 21:20:45 -0500

    :truck: environment setup

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,3 @@
 // FRONTEND SECRETS
-const AUTH_TOKEN = "4ur3y350nly";
-const LINKS_PER_PAGE = 5;
-
-module.exports = {
-  AUTH_TOKEN,
-  LINKS_PER_PAGE
-};
+export const AUTH_TOKEN = "4ur3y350nly";
+export const LINKS_PER_PAGE = 5;
