cd7e1f34edd87cfd24275e53adaf55b01ce1bd94
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-10 23:12:39 -0500

    :rocket: Final commit?
    Done-zo

--- server/src/index.js
+++ server/src/index.js
@@ -28,6 +28,4 @@ const server = new GraphQLServer({
   context: request => ({ ...request, prisma })
 });
 
-server.start({ playground: false }, () =>
-  console.log(`Server is running on http://localhost:4000`)
-);
+server.start(() => console.log(`Server is running on http://localhost:4000`));
--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,23 @@ import Login from "./Login";
 import Search from "./Search";
 import Lost from "./Lost";
 
+import { AUTH_TOKEN } from "../constants";
+
 class App extends Component {
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
+
     return (
       <main className="center basis">
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
             <Route exact path="/" render={() => <Redirect to="/new/1" />} />
-            <Route exact path="/create" component={CreateLink} />
+            <Route
+              exact
+              path="/create"
+              component={authToken ? CreateLink : Login}
+            />
             <Route exact path="/login" component={Login} />
             <Route exact path="/search" component={Search} />
             <Route exact path="/top" component={LinkList} />
