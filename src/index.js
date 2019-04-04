7fa998e9d8cf291d3e4663f38d253d05b1c54c81
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-04-04 19:49:32 -0400

    🎨 Make deploy notes

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 // create-react-app generics
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./components/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
 
 // Required Apollo imports
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
 
 // Required subscription server imports
-import { split } from "apollo-link";
-import { WebSocketLink } from "apollo-link-ws";
-import { getMainDefinition } from "apollo-utilities";
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 // Link to GraphQL Context for setting http headers
-import { setContext } from "apollo-link-context";
-import { AUTH_TOKEN, GRAPHQL_ENDPOINT } from "./constants";
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN, GRAPHQL_ENDPOINT } from './constants'
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker'
 
 // Connect to GraphQL Server
-const httpLink = createHttpLink({
-  uri: `http://${GRAPHQL_ENDPOINT}`
-});
+const httpsLink = createHttpLink({
+  uri: `http://${GRAPHQL_ENDPOINT}` // or https:// when deploying
+})
 
 // Set authentication headers
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN);
+  const token = localStorage.getItem(AUTH_TOKEN)
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ""
+      authorization: token ? `Bearer ${token}` : ''
     }
-  };
-});
+  }
+})
 
 // Establish the WebSocketLink
-const wsLink = new WebSocketLink({
-  uri: `ws://${GRAPHQL_ENDPOINT}`,
+const wssLink = new WebSocketLink({
+  uri: `ws://${GRAPHQL_ENDPOINT}`, // or wss:// when deploying
   options: {
     reconnect: true,
     connectionParams: {
@@ -48,7 +48,7 @@ const wsLink = new WebSocketLink({
       authToken: localStorage.getItem(AUTH_TOKEN)
     }
   }
-});
+})
 
 // Create a split link for both the WebSocket, and Http/Auth
 // 1. param => func : boolean, a test function
@@ -56,18 +56,18 @@ const wsLink = new WebSocketLink({
 // 3. param => link, if (func() === false) return this link
 const link = split(
   ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === "OperationDefinition" && operation === "subscription";
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
   },
-  wsLink,
-  authLink.concat(httpLink)
-);
+  wssLink,
+  authLink.concat(httpsLink)
+)
 
 // Instantiate the ApolloClient
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache()
-});
+})
 
 ReactDOM.render(
   <BrowserRouter>
@@ -75,10 +75,10 @@ ReactDOM.render(
       <App />
     </ApolloProvider>
   </BrowserRouter>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
